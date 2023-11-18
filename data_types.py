e4637bb6c70a6f1b924c094674b26116c06a1073
jprivera44 <jprivera44@gmail.com>
2023-11-17 20:55:05 -0800

    new

--- data_types.py
+++ data_types.py
@@ -4,15 +4,6 @@ from dataclasses import dataclass
 from typing import Optional
 
 
-@dataclass
-class Action:
-    """A message sent from one power to another."""
-
-    name: str
-    self: str
-    other: str
-    content: str
-
 
 @dataclass
 class BackendResponse:
@@ -25,28 +16,3 @@ class BackendResponse:
     total_tokens: int
 
 
-@dataclass
-class NationResponse:
-    """A response from a Nation agent for a single turn."""
-
-    reasoning: str  # Private reasoning to generate the response.
-    actions: list[Action]  # Orders to execute.
-    system_prompt: str  # System prompt
-    user_prompt: Optional[str]  # User prompt if available
-    prompt_tokens: int  # Number of tokens in prompt
-    completion_tokens: int  # Number of tokens in completion
-    total_tokens: int  # Total number of tokens in prompt and completion
-    completion_time_sec: float  # Time to generate completion in seconds
-
-
-@dataclass
-class WorldModelResponse:
-    """A response from the world model about the consequences of a day's actions."""
-
-    consequences: str
-    system_prompt: str  # System prompt
-    user_prompt: Optional[str]  # User prompt if available
-    prompt_tokens: int  # Number of tokens in prompt
-    completion_tokens: int  # Number of tokens in completion
-    total_tokens: int  # Total number of tokens in prompt and completion
-    completion_time_sec: float  # Time to generate completion in seconds
